27fa36837524a289bf0f041d47ce0d55356c9c23
Dang Hoang <danghmh1205@gmail.com>
2025-11-19 19:48:13 +0700

    Initial project setup

---
+++ TravellingMap/TravellingMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravellingMapApp.swift
+//  TravellingMap
+//
+//  Created by Hoàng Minh Hải Đăng on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TravellingMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
