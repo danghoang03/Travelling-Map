21c718154abd604ebbc7aed3dfaaa2cd9bd61f80
Dang Hoang <danghmh1205@gmail.com>
2025-11-20 19:03:17 +0700

    Implement LocationPreviewView UI component

---
+++ TravellingMap/Views/LocationPreviewView.swift
@@ -0,0 +1,87 @@
+//
+//  LocationPreviewView.swift
+//  TravellingMap
+//
+//  Created by Hoàng Minh Hải Đăng on 20/11/25.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    let location: Location
+    
+    var body: some View {
+        HStack(alignment: .bottom) {
+            VStack(alignment: .leading, spacing: 16) {
+                imageSection
+                titleSection
+            }
+            
+            VStack(spacing: 8) {
+                learnMoreButton
+                nextButton
+            }
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+                .offset(y: 65)
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension LocationPreviewView {
+    private var imageSection: some View {
+        ZStack {
+            if let image = location.imageNames.first {
+                Image(image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(location.name)
+                .font(.title2.bold())
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreButton: some View {
+        Button {
+            
+        } label: {
+            Text("Learn More")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button {
+            
+        } label: {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.bordered)
+    }
+}
+
+#Preview {
+    LocationPreviewView(location: LocationsDataService.locations.first!)
+        .padding()
+}
