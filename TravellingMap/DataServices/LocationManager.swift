093e8699553242b35990fba33b30109e03e7045e
Dang Hoang <danghmh1205@gmail.com>
2025-11-23 19:38:26 +0700

    feat(service): create LocationManager for CoreLocation

---
+++ TravellingMap/DataServices/LocationManager.swift
@@ -0,0 +1,32 @@
+//
+//  LocationManager.swift
+//  TravellingMap
+//
+//  Created by Hoàng Minh Hải Đăng on 23/11/25.
+//
+
+import Foundation
+import CoreLocation
+import Observation
+
+@Observable
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationManager()
+    private let manager = CLLocationManager()
+    
+    var authorizationStatus: CLAuthorizationStatus?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func requestLocationPermission() {
+        manager.requestWhenInUseAuthorization()
+    }
+        
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        self.authorizationStatus = manager.authorizationStatus
+    }
+}
